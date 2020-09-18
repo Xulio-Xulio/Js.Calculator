49c8795301eb8f252b6b1ba430645ec021f8e609
Xulio-Xulio <61099971+Xulio-Xulio@users.noreply.github.com>
2020-09-17 22:23:41 -0300

    try%

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-function calcular() {
-    
+function calcular(type, value) {
+    if(type === 'action') {
+        if (value === 'ac'){
+          document.querySelector('input#result').value = ''
+        }
+        if (value === '+' || value === '-' || value === '*' || value === '/' || value === '.' || value === `(` || value === `)`){
+          document.querySelector('input#result').value += value
+        }
+        if (value === '=') {
+          var value_camp = eval(document.querySelector('input#result').value)
+          document.querySelector('input#result').value = value_camp
+        }
+        if (value === `%`) {
+            document.querySelector('input#result').value = (value*value)/100
+            console.log(value)
+        }
+      } else if (type === 'value') {
+
+        document.querySelector('input#result').value += value
+      }
 }
